137de0f33c0efe2abb94834ea11f690a65fdb45e
Md Ariful Islam <arifuldaud@gmail.com>
2026-09-23 08:46:02 +0600

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+
 import './index.css'
-import App from './App.jsx'
+import Homepage from './pages/homepage.jsx'
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Homepage />,
+  },
+  {
+    path: "/features",
+    element: <Homepage />,
+  },
+  {
+    path: "/how-it-works",
+    element: <Homepage />,
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,34 @@
+import AiDayPlanning from "../Components/AiDayPlanning";
+import Banner from "../Components/Banner";
+import Featured from "../Components/Featured";
+import Header from "../Components/Header";
+import HowItWorks from "../Components/HowItWorks";
+import Stats from '../Components/Stats';
+import Studing from "../Components/Studing";
+import Review from "../Components/Review";
+import Pricing from "../Components/Pricing";
+import FAQ from "../Components/FAQ";
+import NextStudySession from "../Components/NextStudySession";
+
+function Homepage() {
+  return (
+    <>
+      <Header />
+      <Banner />
+      <Stats />
+      <Studing />
+      <Featured />
+      <HowItWorks />
+      <AiDayPlanning/>
+      <Review />
+      <Pricing />
+   
+      <FAQ />
+         <NextStudySession />
+         <footer />
+         
+    </>
+  );
+}
+
+export default Homepage;
